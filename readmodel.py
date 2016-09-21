c31a8eb6f9f5890355173b96470df860e542f968
iceshade000 <857332641@qq.com>
2016-09-21 21:14:01 +0800

    已经可以从caffemodel中读取指定层的参数了，利用已有的参数修改caffemodel看起来难度并不大，接下来的任务是将读取的参数存储到neo4j中

--- readmodel.py
+++ readmodel.py
@@ -20,31 +20,33 @@ pf = open(params_txt, 'w')
 net = caffe.Net(MODEL_FILE, PRETRAIN_FILE, caffe.TEST)
 
 # 遍历每一层
-for param_name in net.params.keys():
-    # 权重参数
-    weight = net.params[param_name][0].data
-    # 偏置参数
-    bias = net.params[param_name][1].data
+#for param_name in net.params.keys():
+param_name="InnerProduct2"
+# 权重参数
+weight = net.params[param_name][0].data
+# 偏置参数
+bias = net.params[param_name][1].data
 
-    # 该层在prototxt文件中对应“top”的名称
-    pf.write(param_name)
-    pf.write('\n')
+# 该层在prototxt文件中对应“top”的名称
+pf.write(param_name)
+pf.write('\n')
 
-    # 写权重参数
-    pf.write('\n' + param_name + '_weight:\n\n')
-    # 权重参数是多维数组，为了方便输出，转为单列数组
-    weight.shape = (-1, 1)
+# 写权重参数
+pf.write('\n' + param_name + '_weight:\n\n')
+# 权重参数是多维数组，为了方便输出，转为单列数组
+#weight.shape = (-1, 1)
 
-    for w in weight:
+for i in range(0,9):
+    for w in weight[i]:
         pf.write('%ff, ' % w)
 
     # 写偏置参数
-    pf.write('\n\n' + param_name + '_bias:\n\n')
+    pf.write('\n\n' + str(i) + '_bias:\n\n')
     # 偏置参数是多维数组，为了方便输出，转为单列数组
     bias.shape = (-1, 1)
-    for b in bias:
-        pf.write('%ff, ' % b)
+#for b in bias:
+    pf.write('%ff\n ' % bias[i])
 
-    pf.write('\n\n')
+pf.write('\n\n')
 
 pf.close
