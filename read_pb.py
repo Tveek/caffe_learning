a8e853de2582b45ff855e63ab5ef04e705a7a874
iceshade000 <857332641@qq.com>
2016-09-26 20:40:14 +0800

    成功对参数进行处理,并进行修改和写入

--- read_pb.py
+++ read_pb.py
@@ -12,16 +12,36 @@ MODEL_FILE = 'mnist/deploy.prototxt'
 # 预先训练好的caffe模型
 PRETRAIN_FILE = 'mnist/lenet_iter_9380.caffemodel'
 WRITE_FILE ='mnist/my_lenet.caffemodel'
+WRITE_FILE2='mnist/my_lenet2.caffemodel'
 
 # 保存参数的文件
 params_txt = 'params.txt'
-pf = open(params_txt, 'w')
-input=open(PRETRAIN_FILE,'rb')
+#pf = open(params_txt, 'w')
+input=open(WRITE_FILE,'rb')
+output=open(WRITE_FILE2,'wb')
 
 param=caffe_pb2.NetParameter()
 param.ParseFromString(input.read())
 
+param_name="InnerProduct2"
 #def read_caffemodel(caffefile):
-print param
-pf.close
+
+net=caffe_pb2.NetParameter()
+net=param
+#netlayer=net.layer.add()
+
+for layers in param.layer:
+    if layers.name==param_name:
+        print layers.blobs[0].data[0]
+        layers.blobs[0].data[0]=0
+        #netlayer = layers
+
+net_str=net.SerializeToString()
+
+output.write(net_str)
+
+#成功读取并修改，现在生成
+
+#pf.close
 input.close()
+output.close()
